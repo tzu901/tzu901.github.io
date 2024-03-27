e9370ca855304b7d82b1828569b25046c9af1c83
Tzu901 <108435063+tzu901@users.noreply.github.com>
2024-03-27 17:23:56 +0800

    Add files via upload

--- src/assignment/creditCardChecking/src/index.js
+++ src/assignment/creditCardChecking/src/index.js
@@ -5,28 +5,27 @@ function creditCardChecking() {
     const validationResult = document.getElementById('validation');
     const bank = document.getElementById('bank');
     const creditCard = document.getElementById('creditCard').value.replace(/-/g, '').replace(/ /g, '');
+    console.log(creditCardInput.value);
 
     bank.textContent = ''
     if (creditCard == '') {
         validationResult.textContent = ''
         return;
     }
-
-    if (creditCard[0] !== '3' && creditCard[0] !== '4' && creditCard[0] !== '5' || creditCard.length !== 16) {
+    // (creditCard[0] !== '3' && creditCard[0] !== '4' && creditCard[0] !== '5' || creditCard.length !== 16)
+    if (creditCard[0] !== '3' && creditCard[0] !== '4' && creditCard[0] !== '5') {
         validationResult.textContent = 'Invalid';
         validationResult.style.color = 'red';
+        console.log('Invalid');
+        console.log('Credit card number should start with 3, 4 or 5');
         return;
     }
 
-    if (creditCard[0] == 3) {
-        console.log('American Express or JCB');
-        bank.textContent = 'American Express or JCB';
-    } else if (creditCard[0] == 4) {
-        console.log('Visa');
-        bank.textContent = 'Visa';
-    } else if (creditCard[0] == 5) {
-        console.log('MasterCard');
-        bank.textContent = 'MasterCard';
+    if(creditCard.length !== 16) {
+        validationResult.textContent = 'Invalid';
+        validationResult.style.color = 'red';
+        console.log('Credit card number should be 16 digits long');
+        return;
     }
     
     let total = 0, odd = true;
@@ -47,10 +46,26 @@ function creditCardChecking() {
     if (total % 10 === 0) {
         validationResult.textContent = 'Valid';
         validationResult.style.color = 'green';
+        console.log('Valid');
     } else {
         validationResult.textContent = 'Invalid';
         validationResult.style.color = 'red';
+        console.log('Invalid');
+        console.log('Credit card number total divided by 10 should be 0');
+        return;
     }
+
+    if (creditCard[0] == 3) {
+        console.log('American Express or JCB');
+        bank.textContent = 'American Express or JCB';
+    } else if (creditCard[0] == 4) {
+        console.log('Visa');
+        bank.textContent = 'Visa';
+    } else if (creditCard[0] == 5) {
+        console.log('MasterCard');
+        bank.textContent = 'MasterCard';
+    }
+
 }
     
 document.getElementById('creditCard').addEventListener('change', function() {
